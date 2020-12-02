3ac11fbe123de2e96001bf2ab7b952ce225eddb6
mhdnaldi <mhd.naldi@yahoo.com>
2020-12-02 19:31:46 +0700

    add Person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 // import logo from './logo.svg';
+// import { render } from '@testing-library/react';
+import React, { Component } from 'react'
+import Person from './Person/Person'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi React!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>First day using React!</h1>
+        <p>React is awesome</p>
+        <Person />
+      </div>
+    )
+  }
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi React!</h1>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Person/Person.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = () => {
+  return <h2>I'm a person</h2>
+}
+
+export default person
