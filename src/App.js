864c430003716f04f75bcc146a97d9df7bae825b
mhdnaldi <mhd.naldi@yahoo.com>
2020-12-09 07:08:38 +0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -5,108 +5,78 @@ import "./App.css";
 class App extends Component {
   state = {
     persons: [
-      { name: "Naruto", age: 20 },
-      { name: "Sasuke", age: 21 },
-      { name: "Sakura", age: 21 },
+      { name: "Naruto", age: 23 },
+      { name: "Sasuke", age: 24 },
+      { name: "Sakura", age: 24 },
     ],
+    showPersons: false,
   };
-  switchNameHandler = (name) => {
+
+  btnStyle = {
+    backgroundColor: "white",
+    fontSize: "16px",
+    padding: "16px",
+    margin: "0 10px",
+  };
+
+  changeNameHandler = () => {
     this.setState({
       persons: [
-        { name: name, age: 20 },
-        { name: "Uchiha Sasuke", age: 21 },
-        { name: "Haruno Sakura", age: 21 },
+        { name: "Naruto Uzumaki", age: 23 },
+        { name: "Sasuke Uchiha", age: 24 },
+        { name: "Sakura Haruno", age: 24 },
       ],
     });
   };
-  changeNameHandler = (event) => {
+
+  inputTextHandler = (event) => {
     this.setState({
       persons: [
-        { name: "Uzumaki Naruto", age: 20 },
-        { name: event.target.value, age: 21 }, // EVENT TARGET VALUE UTK MENGAMBIL ISI TEXT YANG DIINPUT
-        { name: "Haruno Sakura", age: 21 },
+        { name: event.target.value, age: 23 },
+        { name: "Sasuke", age: 24 },
+        { name: "Sakura", age: 24 },
       ],
     });
+  };
+
+  togglePersonsHandler = () => {
+    const isSHow = this.state.showPersons;
+    this.setState({ showPersons: !isSHow });
   };
   render() {
-    const btnStyle = {
-      backgroundColor: "white",
-      padding: "16px",
-      font: "inherit",
-      borderColor: "1px solid #eee",
-      cursor: "pointer",
-    };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            change={this.inputTextHandler}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <h1>Team Kakashi</h1>
-        <button
-          style={btnStyle}
-          onClick={() => this.switchNameHandler("Uzumaki naruto")}
-        >
-          Fullname
+        <h1>React Course</h1>
+        <button style={this.btnStyle} onClick={this.changeNameHandler}>
+          Full name
         </button>
-        <Person
-          click={() => this.switchNameHandler("Dattebayo!")}
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          change={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button style={this.btnStyle} onClick={this.togglePersonsHandler}>
+          Show persons
+        </button>
+        {persons}
       </div>
     );
   }
 }
+
 export default App;
-
-// REACT HOOKS
-// import React, { useState } from "react";
-// const App = (props) => {
-//   const [personsState, setPersonsState] = useState({
-//     persons: [
-//       { name: "Kakashi", age: 40 },
-//       { name: "Naruto", age: 23 },
-//       { name: "Sakura", age: 24 },
-//       { name: "Sasuke", age: 24 },
-//     ],
-//   });
-
-//   const switchNameHandler = () => {
-//     setPersonsState({
-//       persons: [
-//         { name: "Kakashi Sensei", age: 40 },
-//         { name: "Naruto Uzumaki", age: 23 },
-//         { name: "Sakura Haruno", age: 24 },
-//         { name: "Sasuke Uchiha", age: 24 },
-//       ],
-//     });
-//   };
-//   return (
-//     <div className="App">
-//       <h1>Team 7</h1>
-//       <button onClick={switchNameHandler}>Switch button</button>
-//       <Person
-//         name={personsState.persons[0].name}
-//         age={personsState.persons[0].age}
-//       />
-//       <Person
-//         name={personsState.persons[1].name}
-//         age={personsState.persons[1].age}
-//       />
-//       <Person
-//         name={personsState.persons[2].name}
-//         age={personsState.persons[2].age}
-//       />
-//       <Person
-//         name={personsState.persons[3].name}
-//         age={personsState.persons[3].age}
-//       />
-//     </div>
-//   );
-// };
